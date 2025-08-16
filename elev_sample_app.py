cbae09fbd107b004304aa2308383a503d57c5e06
RJ Balikian <rileybalikian@outlook.com>
2025-08-16 14:47:00 -0500

    fix to input output with different crs

--- elev_sample_app.py
+++ elev_sample_app.py
@@ -22,9 +22,22 @@ RASTER_SRC_DICT = {"ISGS Statewide Lidar":LIDAR_SERVICE_URL,
                    "Raster file":"get_file_name"
                    }
 
+DEFAULT_POINTS_CRS = "EPSG:6345 - NAD83(2011) / UTM zone 16N"
+DEFAULT_POINTS_CRS_INDEX = CRS_STR_LIST.index(DEFAULT_POINTS_CRS)
+
+DEFAULT_OUTPUT_CRS = DEFAULT_POINTS_CRS
+
 def main():
-    st.session_state.main_container = st.container()
+    print("Start Main Loop")
+    #st.container(key='main_container')
+
+    with st.container():
+        if hasattr(st.session_state, 'elev_fig'):
+            st.plotly_chart(st.session_state.elev_fig)
+            st.session_state.coords_df
+            
     with st.sidebar:
+        print('Building sidebar')
         st.title("Raster Sampling on the Web")
 
         st.selectbox(label="Select raster/elevation data source",
@@ -42,7 +55,7 @@ def main():
                      disabled=True)
 
 
-        raster_expander = st.expander(label='Raster Info.')
+        #raster_expander = st.expander(label='Raster Info.')
         
         st.segmented_control(label='Select point type',
                              options=["Enter coords.",
@@ -68,10 +81,16 @@ def main():
         
         st.selectbox(label="Point CRS",
                      options=CRS_STR_LIST,
-                     index=CRS_STR_LIST.index("EPSG:6345 - NAD83(2011) / UTM zone 16N"),
+                     index=DEFAULT_POINTS_CRS_INDEX,
                      key='point_crs')
 
-        point_expander = st.expander(label='Point Info.')
+        #point_expander = st.expander(label='Point Info.')
+
+        st.divider()
+        st.selectbox(label="Output CRS",
+                     options=CRS_STR_LIST,
+                     index=DEFAULT_POINTS_CRS_INDEX,
+                     key='output_crs')
 
         st.button(label="Sample Elevation Data",
                   key="sample_data_button",
@@ -79,9 +98,9 @@ def main():
                   on_click=get_elevation,
                   type='primary',
                   )
+        print('Done Building sidebar')
+    print("MAIN LOOP END")
 
-    #if hasattr(st.session_state, 'elev_fig'):
-    #    st.session_state.elev_fig
 
 # FUNCTIONS
 def on_raster_source_change():
@@ -103,67 +122,94 @@ def on_xcoord_change():
 def get_elevation(coords=None, 
                   elevation_col_name='elevation', 
                   xcoord_col_name='xcoord', ycoord_col_name='ycoord', 
-                  points_crs=None, 
+                  points_crs=None, output_crs=None,
                   elevation_source=None, elev_source_type='service', 
                   raster_crs=None, show_plot=True):
+
+    print("GETTING ELEVATION")
 
     if coords is None:
         if st.session_state.points_source=='Enter coords.':
             coords = (st.session_state.xcoord, st.session_state.ycoord)
-    
+            #coords = (-88.857362, 42.25637743)
 
     # Leave everything below here alone
     if elevation_source is None:
-        elevation_source = RASTER_SRC_DICT[st.session_state['raster_source_select']]
+        elevation_source = LIDAR_SERVICE_URL#RASTER_SRC_DICT[st.session_state['raster_source_select']]
 
     if points_crs is None:
         points_crs = int(CRS_DICT[st.session_state.point_crs].code)
+        points_crs_name = CRS_DICT[st.session_state.point_crs].name
 
     if raster_crs is None:
         raster_crs = int(CRS_DICT[st.session_state.raster_crs].code)
+        raster_crs_name = CRS_DICT[st.session_state.raster_crs].name
 
+    if output_crs is None:
+        output_crs = int(CRS_DICT[st.session_state.output_crs].code)
+        output_crs_name = CRS_DICT[st.session_state.output_crs].name
+    
     elev_wms = wms_statewide_lidar_url = elevation_source
 
-    coordTransformer = pyproj.Transformer.from_crs(points_crs, raster_crs)
+    ptCoordTransformerOUT = pyproj.Transformer.from_crs(crs_from=points_crs, crs_to=output_crs, always_xy=True)
+    ptCoordTransformerRaster = pyproj.Transformer.from_crs(crs_from=points_crs, crs_to=raster_crs, always_xy=True)
     wms_statewide_lidar_url = elev_wms
 
     if isinstance(coords, (tuple, list)):
         xcoord, ycoord = coords
-        xcoord_WMS, ycoord_WMS = coordTransformer.transform(xcoord, ycoord)
-        minX = maxX = xcoord_WMS
-        minY = maxY = ycoord_WMS
+
+        xcoord_OUT, ycoord_OUT = ptCoordTransformerOUT.transform(xcoord, ycoord)
+        xcoord_RAST, ycoord_RAST = ptCoordTransformerRaster.transform(xcoord, ycoord)
+
+        minX = maxX = xcoord_OUT
+        minY = maxY = ycoord_OUT
+
+        minXRast = maxXRast= xcoord_RAST
+        minYRast = maxYRast = ycoord_RAST
+
+        cols = [f"{points_crs}_xIN", f"{points_crs}_yIN", f"{output_crs}_x", f"{output_crs}_y"]
+        coords = pd.DataFrame([[xcoord, ycoord, xcoord_OUT, ycoord_OUT]], columns=cols)
+
     elif isinstance(coords, (pd.DataFrame, gpd.GeoDataFrame, str, pathlib.Path)):
         if isinstance(coords, (pathlib.Path, str)):
             coords = pd.read_csv(coords)
         xcoord = coords[xcoord_col_name]
         ycoord = coords[ycoord_col_name]
-        xcoord_WMS, ycoord_WMS = coordTransformer.transform(xcoord, ycoord)
 
-        minX = min(xcoord_WMS)
-        minY = min(ycoord_WMS)
-        maxX = max(xcoord_WMS)
-        maxY = max(ycoord_WMS)
+        if str(raster_crs) == '3857':
+            tempCoord = ycoord
+            ycoord = xcoord
+            xcoord = tempCoord
 
+        xcoord_OUT, ycoord_OUT = ptCoordTransformerOUT.transform(xcoord, ycoord)
 
-    xPad = (maxX-minX)*0.1
-    yPad = (maxY-minY)*0.1
+        minX = min(xcoord_OUT)
+        minY = min(ycoord_OUT)
+        maxX = max(xcoord_OUT)
+        maxY = max(ycoord_OUT)
 
-    if xPad == 0:
-        xPad = maxX * 0.01
+    xPad = (maxXRast-minXRast)*0.1
+    yPad = (maxYRast-minYRast)*0.1
+
+    if float(xPad) == 0.0:
+        xPad = maxXRast * 0.01
+        xPad = abs(xPad)
         if abs(xPad) > 1000:
             xPad = 1000
-    if yPad == 0:
-        yPad = maxY * 0.01
-        if abs(yPad) > 1000:
+
+    if float(yPad) == 0.0:
+        yPad = maxYRast * 0.01
+        yPad = abs(yPad)
+        if yPad > 1000:
             yPad = 1000
-        
+
     if elev_source_type=='service':
         wms = WebMapService(wms_statewide_lidar_url)
 
         layer_name = '0'
         layer = wms[layer_name]
 
-        bbox = (minX-xPad, minY-yPad, maxX+xPad, maxY+yPad)
+        bbox = (minXRast-xPad, minYRast-yPad, maxXRast+xPad, maxYRast+yPad)
         img = wms.getmap(
             layers=['IL_Statewide_Lidar_DEM_WGS:None'],
             srs='EPSG:3857',
@@ -173,22 +219,16 @@ def get_elevation(coords=None,
             transparent=True
             )
 
-        try:
-            bio = BytesIO(img.read())
-            lidarData_rxr = rxr.open_rasterio(bio)
-            lidarData = lidarData_rxr.rio.reproject(points_crs)
-        except Exception as e:
-            print(f"BytesIO method failed: {e}")
+        bio = BytesIO(img.read())
+        lidarData_rxr = rxr.open_rasterio(bio)
+        lidarData = lidarData_rxr.rio.reproject(output_crs)
+        if 'band' in lidarData.dims:
+            lidarData = lidarData.isel(band=0)
 
-        #with MemoryFile(img) as memfile:
-            #with memfile.open() as dataset:
-        #        lidarData_rxr = rxr.open_rasterio(dataset)
-        #lidarData = lidarData_rxr.rio.reproject(points_crs)
     elif elev_source_type == 'file':
         lidarData_rxr = rxr.open_rasterio(elevation_source)
-
         lidarData = lidarData_rxr.sel(x=slice(minX-xPad, maxX+xPad), y=slice(maxY+yPad, minY-yPad))
-        lidarData = lidarData.rio.reproject(points_crs)
+        lidarData = lidarData.rio.reproject(output_crs)
     
     minLidarVal = lidarData.min().values
     maxLidarVal = lidarData.max().values
@@ -196,84 +236,93 @@ def get_elevation(coords=None,
     
     vMin = minLidarVal + 0.2*lidarValRange
     vMax = maxLidarVal - 0.2*lidarValRange
-    
-    if isinstance(coords, (tuple, list)):
-        elev_ft = float(lidarData.sel(x=xcoord, y=ycoord, method='nearest').values[0])
-        elev_m = elev_ft * 0.3048
-        st.session_state.main_container.text(f"elev {elev_ft:.2f} ft    | {elev_m:.2f}m")
 
-        if show_plot:
-            #fig, ax = plt.subplots()#figsize=(1,1))
-            #lidarData[0].plot(ax=ax, alpha=0.75, cmap='gist_earth', vmin=vMin, vmax=vMax)
-            data = lidarData[0].values
-            x_coords = lidarData.x.values  
-            y_coords = lidarData.y.values
+    # Elevation in meters
+    lidarData_m = lidarData * 0.3048
 
-            # Create heatmap
-            fig = go.Figure(data=go.Heatmap(
-                z=data,
-                x=x_coords,
-                y=y_coords,
-                colorscale='Geyser',
-                zmin=vMin,
-                zmax=vMax,
-                colorbar=dict(title="Elevation (ft)")
-            ))
-            
-            # Add the point marker
-            fig.add_trace(go.Scatter(
-                x=[xcoord],
-                y=[ycoord],
-                mode='markers+text',
-                marker=dict(
-                    symbol='star',
-                    size=15,
-                    color='red',
-                    line=dict(width=2, color='black')
-                ),
-                text=[f"{elev_ft:.1f} ft<br>{elev_m:.2f} m"],
-                textposition="top right",
-                textfont=dict(color="red", size=12),
-                name=f"Point Elevation",
-                showlegend=True
-            ))
-            
-            # Update layout
-            fig.update_layout(
-                title="Lidar Elevation Data",
-                xaxis_title="X Coordinate",
-                yaxis_title="Y Coordinate",
-                width=700,
-                height=500,
-                showlegend=True
-            )
-            
-            #st.session_state.main_container.pyplot(fig)
-            st.session_state.main_container.plotly_chart(fig)
+    print(lidarData_m)
+    # Calculate elevation and add to df
+    elev_m = []
+    for i, row in coords.iterrows():
+        lidarSel = lidarData_m.copy()
+        xc = row[f"{output_crs}_x"]
+        yc = row[f"{output_crs}_y"]
 
-        coords = (coords[0], coords[1], round(elev_m, 3))
+        elevVal = lidarSel.sel(x=xc, y=yc, 
+                               method='nearest').values
+
+        elev_m.append(elevVal)
         
-    elif isinstance(coords, (pd.DataFrame, gpd.GeoDataFrame)):
-        elev_ft = []
-        for i, x in enumerate(xcoord):
-            y = ycoord[i]
-            elev_ft.append(lidarData.sel(x=x, y=y, method='nearest').values[0])
-        elev_ft = pd.DataFrame(elev_ft)
-        
-        elev_m = elev_ft * 0.3048
-        coords[elevation_col_name] = elev_m
-        
-        if show_plot:
-            fig, ax = plt.subplots()
-            lidarData[0].plot(ax=ax, alpha=0.75, cmap='gist_earth', vmin=vMin, vmax=vMax)
-            ax.scatter(x=coords[xcoord_col_name], y=coords[ycoord_col_name], c='k', marker='*',)
-            #with st.session_state.main_container:
-            #    st.pyplot(fig)
+    coords['Elev_m'] = elev_m
+    coords['Elev_ft'] = coords['Elev_m'] / 0.3048
+
+    if show_plot:
+        lidarDA = lidarData
+        data = lidarDA.values
+        x_coords = lidarDA.x.values  
+        y_coords = lidarDA.y.values
+
+        # Create heatmap
+        fig = go.Figure(data=go.Heatmap(
+            z=data,
+            x=x_coords,
+            y=y_coords,
+            colorscale='Geyser',
+            zmin=vMin,
+            zmax=vMax,
+            name='Elevation',
+        ))
+                
+        #[print(f"{c['Elev_ft']} ft<br>{c['Elev_m']} m") for i, c in coords.iterrows()]
+        # Add the point marker
+        fig.add_trace(go.Scatter(
+            x=coords[f"{output_crs}_x"],
+            y=coords[f"{output_crs}_y"],
+            mode='markers+text',
+            marker=dict(
+                symbol='star',
+                size=15,
+                color='red',
+                line=dict(width=2, color='black')
+            ),
+            text=[f"{float(c['Elev_ft']):.1f} ft<br>{float(c['Elev_m']):.2f} m" for i, c in coords.iterrows()],
+            textposition="top right",
+            textfont=dict(color="red", size=12),
+            name=f"Point Elevation",
+            #showlegend=True
+        ))
+
+        if len(str(int(minX))) > 4:
+            tickFormat = ".0f"
+        else:
+            tickFormat = ".2f"
+
+        # Update layout
+        fig.update_layout(
+            title="Elevation Data",
+            xaxis_title="X Coordinate",
+            yaxis_title="Y Coordinate",
+            yaxis_tickformat=tickFormat,
+            xaxis_tickformat=tickFormat,
+            width=800,
+            height=800,
+            xaxis=dict(scaleanchor='y'),
+            autosize=True,
+            #showlegend=True,
+            coloraxis_colorbar=dict(#yanchor="top", y=1, x=0,
+                                    orientation='h',
+                                    ticks="outside",
+        ))
+
+        #fig.show()
+        #st.session_state.main_container.pyplot(fig)
+        #with st.session_state.main_container:
+        #    st.plotly_chart(fig)
 
     st.session_state.elev_fig = fig
-
+    st.session_state.coords_df = coords
+    st.balloons()
     return #fig, coords
-
 
 
 if __name__ == "__main__":
